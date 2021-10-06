6152eb9fa25853f2f380b22544166318f1d2e8e0
nakayama91 <k.naka10ry@gmail.com>
2021-10-06 13:33:09 +0000

    [finish]topページレイアウト

--- db/migrate/20211006090116_create_books.rb
+++ db/migrate/20211006090116_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :title：string
+      t.string :title
       t.text :body
 
       t.timestamps
